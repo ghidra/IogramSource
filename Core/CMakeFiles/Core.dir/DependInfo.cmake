b3dd2daebbf1e8dfd8dd4455ee369276f20e8c42
jimmy <ghidra@gmail.com>
2017-03-09 00:30:27 -0500

    updated cmake to latest urho3d

---
+++ Core/CMakeFiles/Core.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/IndexUtilities.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/CMakeFiles/Core.dir/IndexUtilities.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/IoComponentBase.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/CMakeFiles/Core.dir/IoComponentBase.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/IoDataTree.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/CMakeFiles/Core.dir/IoDataTree.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/IoGraph.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/CMakeFiles/Core.dir/IoGraph.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/IoInputSlot.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/CMakeFiles/Core.dir/IoInputSlot.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/IoOutputSlot.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/CMakeFiles/Core.dir/IoOutputSlot.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/IoSerialization.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/CMakeFiles/Core.dir/IoSerialization.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/NetworkUtilities.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/CMakeFiles/Core.dir/NetworkUtilities.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/PersistentData.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/CMakeFiles/Core.dir/PersistentData.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/PluginAPI.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Core/CMakeFiles/Core.dir/PluginAPI.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "URHO3D_ANGELSCRIPT"
+  "URHO3D_LOGGING"
+  "URHO3D_LUA"
+  "URHO3D_NAVIGATION"
+  "URHO3D_NETWORK"
+  "URHO3D_PHYSICS"
+  "URHO3D_PROFILING"
+  "URHO3D_THREADING"
+  "URHO3D_URHO2D"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jimmy/projects/urho/Urho_DYN/include"
+  "/home/jimmy/projects/urho/Urho_DYN/include/Urho3D/ThirdParty"
+  "/home/jimmy/projects/urho/Urho_DYN/include/Urho3D/ThirdParty/Bullet"
+  "/home/jimmy/projects/urho/Urho_DYN/include/Urho3D/ThirdParty/Lua"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Geometry/CMakeFiles/Geometry.dir/DependInfo.cmake
@@ -0,0 +1,96 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/ConversionUtilities.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/ConversionUtilities.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_BestFitPlane.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_BestFitPlane.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_ClosestPoint.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_ClosestPoint.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_ConstructTransform.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_ConstructTransform.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_GeoConversions.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_GeoConversions.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_HausdorffDistance.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_HausdorffDistance.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_Incenter.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_Incenter.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_JoinMeshes.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_JoinMeshes.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_MeshTetrahedralize.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_MeshTetrahedralize.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_PolylineBlend.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_PolylineBlend.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_PolylineDivide.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_PolylineDivide.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_PolylineExtrude.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_PolylineExtrude.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_PolylineLoft.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_PolylineLoft.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_PolylineOffset.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_PolylineOffset.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_PolylinePointFromParameter.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_PolylinePointFromParameter.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_PolylinePointToTransform.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_PolylinePointToTransform.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_PolylineRefine.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_PolylineRefine.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_PolylineTangentVector.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_PolylineTangentVector.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_RayTriangleIntersection.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_RayTriangleIntersection.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_RebuildPolyline.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_RebuildPolyline.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_RemoveDuplicates.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_RemoveDuplicates.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_Rhodo.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_Rhodo.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_SmoothPolyline.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_SmoothPolyline.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_SunPosition.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_SunPosition.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TransformVertexList.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TransformVertexList.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshAverageEdgeLength.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshAverageEdgeLength.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshClosestPoint.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshClosestPoint.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshEdgeCollapse.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshEdgeCollapse.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshEdgeSplit.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshEdgeSplit.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshFrame.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshFrame.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshLoopSubdivide.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshLoopSubdivide.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshMeanCurvatureFlow.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshMeanCurvatureFlow.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshOffset.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshOffset.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshPlaneIntersection.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshPlaneIntersection.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshSaveOFF.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshSaveOFF.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshSubdivide.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshSubdivide.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshThicken.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshThicken.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshVolume.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshVolume.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriMeshWindow.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriMeshWindow.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriTriIntersection.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriTriIntersection.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Geomlib_TriangulatePolygon.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Geomlib_TriangulatePolygon.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/MeshTopologyQueries.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/MeshTopologyQueries.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/NMesh.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/NMesh.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/Polyline.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/Polyline.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/StockGeometries.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/StockGeometries.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/TriMesh.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/TriMesh.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/ThirdParty/ShapeOp/ShapeOp_API.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/__/ThirdParty/ShapeOp/ShapeOp_API.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/ThirdParty/ShapeOp/ShapeOp_Constraint.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/__/ThirdParty/ShapeOp/ShapeOp_Constraint.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/ThirdParty/ShapeOp/ShapeOp_Force.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/__/ThirdParty/ShapeOp/ShapeOp_Force.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/ThirdParty/ShapeOp/ShapeOp_IogramWrapper.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/__/ThirdParty/ShapeOp/ShapeOp_IogramWrapper.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/ThirdParty/ShapeOp/ShapeOp_LSSolver.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/__/ThirdParty/ShapeOp/ShapeOp_LSSolver.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/ThirdParty/ShapeOp/ShapeOp_Solver.cpp" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/__/ThirdParty/ShapeOp/ShapeOp_Solver.cpp.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/ThirdParty/TetGen/predicates.cxx" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/__/ThirdParty/TetGen/predicates.cxx.o"
+  "/home/jimmy/projects/urho/urho_iogram/src/iogram/ThirdParty/TetGen/tetgen.cxx" "/home/jimmy/projects/urho/urho_iogram/src/iogram/Geometry/CMakeFiles/Geometry.dir/__/ThirdParty/TetGen/tetgen.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "URHO3D_ANGELSCRIPT"
+  "URHO3D_LOGGING"
+  "URHO3D_LUA"
+  "URHO3D_NAVIGATION"
+  "URHO3D_NETWORK"
+  "URHO3D_PHYSICS"
+  "URHO3D_PROFILING"
+  "URHO3D_THREADING"
+  "URHO3D_URHO2D"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jimmy/projects/urho/Urho_DYN/include"
+  "/home/jimmy/projects/urho/Urho_DYN/include/Urho3D/ThirdParty"
+  "/home/jimmy/projects/urho/Urho_DYN/include/Urho3D/ThirdParty/Bullet"
+  "/home/jimmy/projects/urho/Urho_DYN/include/Urho3D/ThirdParty/Lua"
+  "src/iogram/Geometry/../ThirdParty"
+  "src/iogram/Geometry/../ThirdParty/Libigl"
+  "src/iogram/Geometry/../ThirdParty/Eigen"
+  "src/iogram/Geometry/../ThirdParty/TetGen"
+  "src/iogram/Geometry/."
+  "src/iogram/Geometry/../ThirdParty/Assimp/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
